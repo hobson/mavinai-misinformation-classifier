b81f856701a71b036a80fcaaccb1885d3f7a6eb0
Hobson Lane <hobson@tangibleai.com>
2021-03-04 22:57:12 -0600

    adding a pipeline

--- src/mavinai_misinformation_classifier/basic_pipeline.py
+++ src/mavinai_misinformation_classifier/basic_pipeline.py
@@ -1,8 +1,7 @@
 """ LogisticRegression with preprocessor to extract text length as only feature
 
->>> training_set = pd.DataFrame([
+>>> X = pd.DataFrame([
 ...     dict(
-...         text='Lorem ipsum article text',
 ...         score_accountability=0.0,
 ...         score_unobjectivity=0.2,
 ...         score_inaccuracy=0.4,
@@ -26,5 +25,53 @@ import pandas as pd  # noqa
 from sklearn.pipeline import Pipeline
 
 
-class SimpleLogisticRegressionPipeline(Pipeline):
+class TextStatsTransformer(TransformerMixin, BaseEstimator):
+    def __init__(self, functions=[len]):
+        self.functions = functions
+
+    def fit(self, X, y=None):
+        """Learn the idf vector (global term weights)
+
+        Inputs:
+          X (sequence of str): array of text strings
+        """
+        # raise NotImplementedError("TextStatsTransformer does not require fitting")
+        return self
+
+    def transform(self, X, copy=True, columns=None, index=None):
+        X = pd.DataFrame(X)
+        if columns is not None:
+            X.columns = columns
+        for fun in functions:
+            for c in columns:
+                try:
+                    X[f"{c}_{fun.__name__}"] = X[c].str.getattr(fun.__name__)()
+                except:
+
+
+        return X
+
+    @property
+    def idf_(self):
+        # if _idf_diag is not set, this will raise an attribute error,
+        # which means hasattr(self, "idf_") is False
+        return np.ravel(self._idf_diag.sum(axis=0))
+
+    @idf_.setter
+    def idf_(self, value):
+        value = np.asarray(value, dtype=np.float64)
+        n_features = value.shape[0]
+        self._idf_diag = sp.spdiags(value, diags=0, m=n_features,
+                                    n=n_features, format='csr')
+
+    def _more_tags(self):
+        return {'X_types': 'sparse'}
+
     pass
+
+
+from sklearn.feature_extraction import text
+
+
+def build_pipeline():
+    return Pipeline([])
